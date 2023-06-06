b734910d113d3713b0a3b498675002637da9bed8
sanu kumar <snkmr13@gmail.com>
2023-06-06 12:18:51 +0530

    Learning CommonJs Module

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-console.log("Hello world");
+const personB = require('./second');
+
+console.log("Hello world", personB);
+console.log(personB.name);
 const a = 30;
 console.log(a /2);
 console.log(__dirname);
---
+++ second.js
@@ -0,0 +1,15 @@
+// (function (exports, require, module, __filename, __dirname) {
+
+    person = {
+        name: "Sanu Kumar",
+        age: '26',
+        company: 'Publicis Sapient'
+    }
+    
+    module.exports = person;
+
+    console.log(exports, require, module, __filename, __dirname)
+// })
+
+
+
